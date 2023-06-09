f804021812d1e9c31c0cad3b99e843bf8cd12482
Nicolnegg <princesnegg@gmail.com>
2023-06-08 21:15:47 -0500

    otro

--- main.py
+++ main.py
@@ -3,19 +3,26 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 # Ruta al archivo de audio .mp3
-archivo_mp3 = "ruta/al/archivo.mp3"
+archivo_mp3 = "canciones/cancion1.mp3"
+
 
 # Cargar el archivo de audio con librosa
-audio, _ = librosa.load("canciones/cancion1.mp3")
+audio, sr = librosa.load(archivo_mp3)
 
-# Obtener la forma de onda
-forma_onda = librosa.amplitude_to_db(np.abs(librosa.stft(audio)), ref=np.max)
+# Obtener la duración del archivo
+duracion = librosa.get_duration(y=audio, sr=sr)
 
+# Generar el arreglo de tiempo
+tiempo = np.linspace(0, duracion, len(audio))
 
 # Graficar la forma de onda
 plt.figure(figsize=(14, 5))
-librosa.display.waveplot(audio, alpha=0.5)
+plt.plot(tiempo, audio, alpha=0.5)
 plt.title('Forma de Onda')
 plt.xlabel('Tiempo (s)')
 plt.ylabel('Amplitud')
+
+# Establecer los límites del eje x
+plt.xlim(0, duracion)
+
 plt.show()
---
+++ porcentaje.py
@@ -0,0 +1,28 @@
+import librosa
+import numpy as np
+
+# Rutas a las canciones en formato de audio
+cancion1 = "canciones/cancion1.wav"
+cancion2 = "canciones/cancion1.wav"
+
+# Cargar las canciones con librosa
+audio1, sr1 = librosa.load(cancion1)
+audio2, sr2 = librosa.load(cancion2)
+
+# Preprocesamiento si es necesario
+
+# Extraer características musicales relevantes
+caracteristicas1 = librosa.feature.chroma_cqt(audio1, sr=sr1)
+caracteristicas2 = librosa.feature.chroma_cqt(audio2, sr=sr2)
+
+# Calcular la distancia entre las características
+distancia = np.linalg.norm(caracteristicas1 - caracteristicas2)
+
+# Establecer un umbral para determinar si las canciones son similares
+umbral = 100  # Ejemplo de umbral, ajustar según tus necesidades
+
+# Comparar la similitud y mostrar el resultado
+if distancia < umbral:
+    print("Las canciones son similares.")
+else:
+    print("Las canciones no son similares.")
